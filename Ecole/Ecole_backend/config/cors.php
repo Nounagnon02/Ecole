19675c92644165690a051fd4d28af2c0b2c287db
Nounagnon02 <princekangbode@gmail.com>
2025-07-23 09:49:45 +0100

    Tentative pour donner les acces 9

--- Ecole/Ecole_backend/config/cors.php
+++ Ecole/Ecole_backend/config/cors.php
@@ -2,10 +2,17 @@
 // Ecole/Ecole_backend/config/cors.php
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'cinetpay/*'],
-    'allowed_methods' => ['*'],
+    'allowed_methods' => [
+        'GET',
+        'POST',
+        'PUT',
+        'PATCH',
+        'DELETE',
+        'OPTIONS'
+    ],
     'allowed_origins' => [
         'http://localhost:3000',
-        'https://ecole-production-2c90.up.railway.app', // HTTPS seulement
+        /*'https://ecole-production-2c90.up.railway.app',*/ // HTTPS seulement
         'https://ecole-one.vercel.app'
     ],
     'allowed_headers' => ['*'],
