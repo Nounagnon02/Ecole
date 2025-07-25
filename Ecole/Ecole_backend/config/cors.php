84846cffe1e713d05da1e80a176c1872ddd57b07
Nounagnon02 <princekangbode@gmail.com>
2025-07-25 09:10:50 +0100

    Modification du fichier cors.php

--- Ecole/Ecole_backend/config/cors.php
+++ Ecole/Ecole_backend/config/cors.php
@@ -2,13 +2,20 @@
 // Ecole/Ecole_backend/config/cors.php
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register', 'cinetpay/*'],
-    'allowed_methods' => [
+    /*'allowed_methods' => [
         'GET',
         'POST',
         'PUT',
         'PATCH',
         'DELETE',
         'OPTIONS'
+    ],*/
+    'allowed_methods' => ['*'], 
+    
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://ecoleprojet.up.railway.app',
+        'https://ecole-one.vercel.app'
     ],
     'allowed_origins' => [
         'http://localhost:3000',
