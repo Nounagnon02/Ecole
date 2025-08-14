632a372013f6ab9ee3b63ebc418b9281610efdfc
Nounagnon02 <princekangbode@gmail.com>
2025-08-14 13:27:21 +0100

    linking front to back

--- Ecole/Ecole_backend/config/sanctum.php
+++ Ecole/Ecole_backend/config/sanctum.php
@@ -19,7 +19,7 @@ return [
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
-        env('APP_URL', 'https://ecoleprojet.up.railway.app'),
+        env('APP_URL', 'https://ecole-2.onrender.com'),
         env('FRONTEND_URL', 'https://ecole-projet.vercel.app'),
 
     ))),
