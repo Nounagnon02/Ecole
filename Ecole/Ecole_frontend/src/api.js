ddfb01aec6898c318ce1728c1123cb2d4700864d
Nounagnon02 <princekangbode@gmail.com>
2025-08-15 17:39:47 +0100

    Linking resolve

--- Ecole/Ecole_frontend/src/api.js
+++ Ecole/Ecole_frontend/src/api.js
@@ -6,7 +6,10 @@ export const urlBase = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 // Axios instance with baseURL
 const api = axios.create({
   baseURL: urlBase,
-  headers: { 'Content-Type': 'application/json' }
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
 
 export default api;
